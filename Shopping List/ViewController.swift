79860337d4b113866a0b98a998108739ba55741d
Moroz Pavel <vferme2015@gmail.com>
2020-04-18 14:55:43 +0300

    Initial Commit

---
+++ Shopping List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping List
+//
+//  Created by Pavel Moroz on 18.04.2020.
+//  Copyright © 2020 Pavel Moroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
